16795e5ae27c32e0b96c5fa84171e487cbe0fb50
cfernando415 <ca.fernando415@gmail.com>
2019-03-20 17:47:07 -0400

    patch index action of diseases controller

--- app/controllers/diseases/api/v1/diseases_controller.rb
+++ app/controllers/diseases/api/v1/diseases_controller.rb
@@ -1,6 +1,7 @@
 class Diseases::Api::V1::DiseasesController < ApplicationController
+    skip_before_action :authorized, only: [:index, :show, :create, :update, :destroy]
     def index
-        @diseases = Disease.select("id, year, leading_cause, sum(deaths) AS deaths, sex").group("year, leading_cause, sex")
+        @diseases = Disease.select("id, year, leading_cause, sum(deaths) AS deaths, sex").group("year, leading_cause, sex, id")
         render json: @diseases
     end
     def show
